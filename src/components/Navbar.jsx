3b7a36bd24173a8cc7e7eefdb21ecee4d9b577a3
TeachDian <philiptenibas@gmail.com>
2024-07-13 15:04:01 +0800

    sd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false); // State for mobile menu
@@ -61,62 +60,86 @@ const Navbar = () => {
 
   return (
     <div
-      className={`flex justify-between items-center h-24 max-w-[1400px] mx-auto px-4 text-[#] ${
-        isSticky ? "fixed inset-x-0 top-0 z-50 bg-[#ffffff] justify-between items-center h-24 max-w-[1400px] mx-auto px-4 text-[#]" : ""
+      className={`flex justify-between items-center h-24 w-full mx-auto px-4 text-[#] ${
+        isSticky
+          ? "fixed inset-x-0 top-0 z-50 bg-[#ffffff] justify-between items-center h-24 w-full mx-auto px-4 text-[#]"
+          : ""
       }`}
     >
-      <h1 className="w-full text-3xl font-bold text-[#424242]">
-        <a href="/">
-          <span style={{ color: "#00003C" }}>Fish</span>
-          <span style={{ color: "#ADD1E9" }}>Lens</span>
-        </a>
-      </h1>
-      <ul className="hidden md:flex items-center">
-        {sections.map((section) => (
-          <li
-            key={section}
-            className={`p-4 font-medium ${activeSection === section ? "text-[#ADD1E9]" : ""}`}
-          >
-            <a href={`/#${section}`}>{section.charAt(0).toUpperCase() + section.slice(1)}</a>
-          </li>
-        ))}
-        <li className="p-4 font-medium">
-          <a href="/register" className="button bg-[#00003C] font-medium h-10 text-white px-5 rounded-md text-[14px]">
-            REGISTER
-          </a>
-        </li>
-      </ul>
-
-      <div onClick={handleNav} className="block md:hidden">
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-      </div>
-
-      <ul
-        className={
-          nav
-            ? "fixed inset-x-0 top-0 h-full bg-[#ffffff] ease-in-out duration-500"
-            : "ease-in-out duration-500 fixed left-[-100%]"
-        }
+      <div
+        className={`flex justify-between items-center h-24 max-w-[1400px] mx-auto px-4 text-[#] ${
+          isSticky
+            ? "fixed inset-x-0 top-0 z-50 bg-[#ffffff] justify-between items-center h-24 max-w-[1400px] mx-auto px-4 text-[#]"
+            : ""
+        }`}
       >
-        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">
-          <span style={{ color: "#00003C" }}>Fish</span>
-          <span style={{ color: "#ADD1E9" }}>Lens</span>
-        </h1>
-        {sections.map((section) => (
-          <li
-            key={section}
-            className={`p-4 border-b border-gray-600 ${activeSection === section ? "text-[#ADD1E9]" : ""}`}
-            onClick={handleNav}
-          >
-            <a href={`#${section}`}>{section.charAt(0).toUpperCase() + section.slice(1)}</a>
-          </li>
-        ))}
-        <li className="p-4 border-b border-gray-600" onClick={handleNav}>
-          <a href="/register" className="button bg-[#00003C] font-medium h-10 text-white px-5 rounded-md text-[14px]">
-            REGISTER
+        <h1 className="w-full text-3xl font-bold text-[#424242]">
+          <a href="/">
+            <span style={{ color: "#00003C" }}>Fish</span>
+            <span style={{ color: "#ADD1E9" }}>Lens</span>
           </a>
-        </li>
-      </ul>
+        </h1>
+        <ul className="hidden md:flex items-center">
+          {sections.map((section) => (
+            <li
+              key={section}
+              className={`p-4 font-medium ${
+                activeSection === section ? "text-[#ADD1E9]" : ""
+              }`}
+            >
+              <a href={`/#${section}`}>
+                {section.charAt(0).toUpperCase() + section.slice(1)}
+              </a>
+            </li>
+          ))}
+          <li className="p-4 font-medium">
+            <a
+              href="/register"
+              className="button bg-[#00003C] font-medium h-10 text-white px-5 rounded-md text-[14px]"
+            >
+              REGISTER
+            </a>
+          </li>
+        </ul>
+
+        <div onClick={handleNav} className="block md:hidden">
+          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        </div>
+
+        <ul
+          className={
+            nav
+              ? "fixed inset-x-0 top-0 h-full bg-[#ffffff] ease-in-out duration-500"
+              : "ease-in-out duration-500 fixed left-[-100%]"
+          }
+        >
+          <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">
+            <span style={{ color: "#00003C" }}>Fish</span>
+            <span style={{ color: "#ADD1E9" }}>Lens</span>
+          </h1>
+          {sections.map((section) => (
+            <li
+              key={section}
+              className={`p-4 border-b border-gray-600 ${
+                activeSection === section ? "text-[#ADD1E9]" : ""
+              }`}
+              onClick={handleNav}
+            >
+              <a href={`#${section}`}>
+                {section.charAt(0).toUpperCase() + section.slice(1)}
+              </a>
+            </li>
+          ))}
+          <li className="p-4 border-b border-gray-600" onClick={handleNav}>
+            <a
+              href="/register"
+              className="button bg-[#00003C] font-medium h-10 text-white px-5 rounded-md text-[14px]"
+            >
+              REGISTER
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
